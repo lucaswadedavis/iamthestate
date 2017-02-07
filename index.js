6981dd3ff4ced8b8352066e0e1f2de35f2a6b746
lucaswadedavis <lucaswadedavis@gmail.com>
2017-02-07 07:02:06 -0800

    Return an object instead of an array.

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
     const iframe = document.createElement('iframe');
     iframe.style.display = 'none';
     document.body.appendChild(iframe);
-    
-    const startingPoint = Object.getOwnPropertyNames(window);
-    const keys = startingPoint.filter(function(prop) {
-        return !iframe.contentWindow.hasOwnProperty(prop);
-    });
-    console.log(keys);
+    const precursors = new Set(Object.getOwnPropertyNames(iframe.contentWindow));
     document.body.removeChild(iframe);
+
+    function iAmTheState() {
+      const current = Object.getOwnPropertyNames(window);
+      let difference = {};
+      for (let i = 0; i < current.length; i++) {
+        if (precursors.has(current[i])) continue;
+        difference[current[i]] = window[current[i]];
+      }
+      return difference;
+    };
+
+    window.iAmTheState = iAmTheState;
 }());
